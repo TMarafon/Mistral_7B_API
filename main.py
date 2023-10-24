19efe9a6493480c74a2676e9c1845d2832f48fc4
Thiago Marafon <tmarafon2@users.noreply.huggingface.co>
2023-10-24 05:48:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from huggingface_hub import InferenceClient
 import os
 
@@ -6,24 +7,20 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    client = InferenceClient(
-        model="mistralai/Mistral-7B-Instruct-v0.1",
-        token=os.environ.get("HF_TOKEN")
-    )
-
-    prompt = """[INST] Hello world!  [/INST]"""
-
-    res = client.text_generation(prompt, max_new_tokens=95)
-    return res
+    prompt = """[INST] Tell me a story about space conquer. [/INST]""".format(input)
+    return StreamingResponse(streamInference(prompt))
 
 @app.get("/{input}")
 async def inference(input):
+    prompt = """[INST] {0} [/INST]""".format(input)
+    return StreamingResponse(streamInference(prompt))
+
+async def streamInference(prompt):
     client = InferenceClient(
         model="mistralai/Mistral-7B-Instruct-v0.1",
         token=os.environ.get("HF_TOKEN")
     )
     
-    prompt = """[INST] {0} [/INST]""".format(input)
-    res = client.text_generation(prompt, max_new_tokens=95)
-    return res
-
+    res = client.text_generation(prompt, max_new_tokens=200, stream=True)
+    for r in res: 
+      yield r.token.text
