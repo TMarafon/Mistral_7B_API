08598cd93ace8a2e4107947bc8f582754bf9cb81
Thiago Marafon <tmarafon2@users.noreply.huggingface.co>
2023-10-24 23:05:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from huggingface_hub import InferenceClient
 
+from langchain.llms import HuggingFaceHub
+
 from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceHubEmbeddings
+from langchain.vectorstores import Chroma
+
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+
+from langchain.chains import RetrievalQA
 
 
 import os
@@ -11,8 +21,26 @@ def prepareVectorDatabase():
     loader = PyPDFLoader(
         'files/Linkedin.pdf'    
     )
-    docs = loader.load()
-    return docs[0].page_content[:500]
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=100)
+    docs = text_splitter.split_documents(documents)
+    embeddings = HuggingFaceHubEmbeddings()
+    db = Chroma.from_documents(texts, embeddings)
+    #TODO: use ContextualCompressionRetriever with MMR
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+    llm = HuggingFaceHub(
+        repo_id="mistralai/Mistral-7B-Instruct-v0.1", 
+        model_kwargs={"temperature":0.1, "max_new_tokens":250}
+    )
+    global qa 
+    qa = RetrievalQA.from_chain_type(
+        llm=llm, 
+        chain_type="stuff", 
+        retriever=retriever, 
+        return_source_documents=True
+    )
+    return "Ready"
+
 
 async def streamInference(prompt):
     client = InferenceClient(
