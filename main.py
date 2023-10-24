2e038154322c392fd0c42406e5cb96867ea247aa
Thiago Marafon <tmarafon2@users.noreply.huggingface.co>
2023-10-24 04:59:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from huggingface_hub import InferenceClient
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello word"}
+    return {"message": "Hello world"}
 
 @app.get("/{input}")
 async def inference(input):
-    return "Your input: " + input
+    client = InferenceClient("mistralai/Mistral-7B-Instruct-v0.1")
+    prompt = """<s>[INST] {0} [/INST]</s>""".format(input)
+    res = client.text_generation(prompt, max_new_tokens=95)
+    return res
