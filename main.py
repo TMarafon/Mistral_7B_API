ff5834228fd0c872125d63cf7a61bad5d73db809
Thiago Marafon <tmarafon2@users.noreply.huggingface.co>
2023-10-24 05:51:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from fastapi.responses import StreamingResponse
 from huggingface_hub import InferenceClient
 import os
 
+async def streamInference(prompt):
+    client = InferenceClient(
+        model="mistralai/Mistral-7B-Instruct-v0.1",
+        token=os.environ.get("HF_TOKEN")
+    )
+    
+    res = client.text_generation(prompt, max_new_tokens=200, stream=True)
+    for r in res: 
+      yield r
+
 app = FastAPI()
 
 @app.get("/")
@@ -15,12 +25,3 @@ async def inference(input):
     prompt = """[INST] {0} [/INST]""".format(input)
     return StreamingResponse(streamInference(prompt))
 
-async def streamInference(prompt):
-    client = InferenceClient(
-        model="mistralai/Mistral-7B-Instruct-v0.1",
-        token=os.environ.get("HF_TOKEN")
-    )
-    
-    res = client.text_generation(prompt, max_new_tokens=200, stream=True)
-    for r in res: 
-      yield r.token.text
