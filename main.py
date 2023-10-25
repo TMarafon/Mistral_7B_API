0b477a2c49ce8269420a3b4891d08fb873819daa
Thiago Marafon <tmarafon2@users.noreply.huggingface.co>
2023-10-25 05:22:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def prepareVectorDatabase():
     #        })
     #    )
     retriever = db.as_retriever(
-        search_type = "mmr", search_kwargs={
+        search_kwargs={
             "k":3,
             "score_threshold": .85
         }
